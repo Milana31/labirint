351f62cd6a291ffcd65ad44ab00988e8d1f71f55
Бексултан <beksultan.erkinbaev@alatoo.edu.kg>
2025-04-15 15:21:47 +0600

    инициализация данных лабиринта

--- LabyrinthEngine.java
+++ LabyrinthEngine.java
@@ -1,8 +1,23 @@
+import java.util.*;
 class LabyrinthEngine {
+    private char[][] grid;
+    private boolean[][] tracker;
+    private int height, width;
+    private int[] origin, destination;
+
     public static void main(String[] args) {
     }
 
-    private void initialize() {}
-    private void createLabyrinth() {}
-    private void resolvePath() {}
+    private void initialize() {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter maze height (>9): ");
+            height = input.nextInt();
+            System.out.print("Enter maze width (>9): ");
+            width = input.nextInt();
+        }
+    }
+    private void createLabyrinth () {
+    }
+    private void resolvePath () {
+    }
 }
